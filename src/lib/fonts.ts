1285695a3c93d0bc8baf3c4f439001265fda76c4
NaotoAzazel <danirucakov@gmail.com>
2025-06-29 11:38:09 +0000

    initial commit

---
+++ src/lib/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Manrope } from 'next/font/google'
+
+export const fontHeading = Manrope({
+  subsets: ['latin'],
+  variable: '--font-heading',
+})
+
+export const fontText = Inter({
+  subsets: ['latin'],
+  variable: '--font-primary',
+})
